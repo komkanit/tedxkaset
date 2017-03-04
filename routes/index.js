b1cf6593733c35da3960bdcb0a2a87462e1e48b0
Ville Vaarala <ville.l.vaarala@gmail.com>
2017-03-04 17:09:01 +0700

    Add speakers page

--- routes/index.js
+++ routes/index.js
@@ -38,6 +38,7 @@ exports = module.exports = function (app) {
 	app.get('/blog/:category?', routes.views.blog);
 	app.get('/blog/post/:post', routes.views.post);
 	app.get('/gallery', routes.views.gallery);
+	app.get('/speakers', routes.views.speakers);
 	app.all('/contact', routes.views.contact);
 
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
--- routes/middleware.js
+++ routes/middleware.js
@@ -22,6 +22,7 @@ exports.initLocals = function (req, res, next) {
 		{ label: 'Home', key: 'home', href: '/' },
 		{ label: 'Blog', key: 'blog', href: '/blog' },
 		{ label: 'Gallery', key: 'gallery', href: '/gallery' },
+		{ label: 'Speakers', key: 'speakers', href: '/speakers' },
 		{ label: 'Contact', key: 'contact', href: '/contact' },
 	];
 	res.locals.user = req.user;
