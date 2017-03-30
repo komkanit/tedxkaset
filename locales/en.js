58ebb7690e97049b345c1a4d0f7092d242b91033
Ville Vaarala <ville.l.vaarala@gmail.com>
2017-03-30 23:50:16 +0700

    Refactor keys for static translations

--- locales/en.js
+++ locales/en.js
@@ -1,23 +1,12 @@
 {
-	"hello": "hello",
-	"navigation": {
-		"about": "about our awesome company"
-	},
-	"Hello": "Is this a hello?",
-	"Welcome": "Welcome",
-	"This is your new %s website.": "This is your new %s website.",
-	"This is your new {{link}} website.": "This is your new {{link}} website.",
-	"This is your new {{{link}}} website.": "This is your new {{{link}}} website.",
-	"This is your new {{{keystoneLink}}} website.": "This is your new {{{keystoneLink}}} website.",
-	"It includes the latest versions of {{{bootstrapLink}}} and {{{jQueryLink}}}.": "It includes the latest versions of {{{bootstrapLink}}} and {{{jQueryLink}}}.",
-	"Visit the {{{keystoneGuideLink}}} guide to learn how to customise it.": "Visit the {{{keystoneGuideLink}}} guide to learn how to customise it.",
-	"We have created a default Admin user for you with the email <strong>tedxku@keystonejs.com</strong> and the password <strong>1q2w3e4r</strong>.": "We have created a default Admin user for you with the email <strong>tedxku@keystonejs.com</strong> and the password <strong>1q2w3e4r</strong>.",
-	"We have created a default Admin user for you with the email {{{email}}} and the password {{{password}}}.": "We have created a default Admin user for you with the email {{{email}}} and the password {{{password}}}.",
-	"Sign into use the Admin UI.": "Sign into use the Admin UI.",
-	"Sign in": "Sign in",
-	"to use the Admin UI.": "to use the Admin UI.",
-	"Star KeystoneJS on GitHub": "Star KeystoneJS on GitHub",
-	"follow @keystonejs": "follow @keystonejs",
-	"Remember to {{{starKeystoneJS}}} and {{{follow}}} on twitter for updates.": "Remember to {{{starKeystoneJS}}} and {{{follow}}} on twitter for updates.",
-	"Open the Admin UI": "Open the Admin UI"
+	"index": {
+		"new_website": "This is your new {{{keystoneLink}}} website.",
+		"libraries": "It includes the latest versions of {{{bootstrapLink}}} and {{{jQueryLink}}}.",
+		"keystone_guide": "Visit the {{{keystoneGuideLink}}} guide to learn how to customise it.",
+		"open_admin_ui": "Open the Admin UI",
+		"greeting": "Welcome",
+		"star": "Star KeystoneJS on GitHub",
+		"star_and_follow": "Remember to {{{starKeystoneJS}}} and {{{follow}}} on twitter for updates.",
+		"follow": "follow @keystonejs"
+	}
 }
--- locales/th.js
+++ locales/th.js
@@ -1,17 +1,16 @@
 {
-	"Hello": "Sawadee",
-	"Speakers": "Speakers in thai",
-	"Welcome": "Welcome",
-	"This is your new {{{keystoneLink}}} website.": "นี่คือเว็บไซต์ {{{keystoneLink}}} ใหม่ของคุณ",
-	"It includes the latest versions of {{{bootstrapLink}}} and {{{jQueryLink}}}.": "It includes the latest versions of {{{bootstrapLink}}} and {{{jQueryLink}}}.",
-	"Visit the {{{keystoneGuideLink}}} guide to learn how to customise it.": "Visit the {{{keystoneGuideLink}}} guide to learn how to customise it.",
-	"We have created a default Admin user for you with the email <strong>tedxku@keystonejs.com</strong> and the password <strong>1q2w3e4r</strong>.": "We have created a default Admin user for you with the email <strong>tedxku@keystonejs.com</strong> and the password <strong>1q2w3e4r</strong>.",
-	"We have created a default Admin user for you with the email {{{email}}} and the password {{{password}}}.": "We have created a default Admin user for you with the email {{{email}}} and the password {{{password}}}.",
-	"Sign into use the Admin UI.": "Sign into use the Admin UI.",
-	"Sign in": "Sign in",
-	"to use the Admin UI.": "to use the Admin UI.",
+	"index": {
+		"new_website": "index.new_website",
+		"libraries": "index.libraries",
+		"keystone_guide": "index.keystone_guide",
+		"open_admin_ui": "index.open_admin_ui",
+		"star_and_follow": {
+			"star": "index.star_and_follow.star"
+		},
+		"greeting": "index.greeting",
+		"star": "index.star",
+		"follow": "index.follow"
+	},
 	"Star KeystoneJS on GitHub": "Star KeystoneJS on GitHub",
-	"follow @keystonejs": "follow @keystonejs",
-	"Remember to {{{starKeystoneJS}}} and {{{follow}}} on twitter for updates.": "Remember to {{{starKeystoneJS}}} and {{{follow}}} on twitter for updates.",
-	"Open the Admin UI": "Open the Admin UI"
+	"follow @keystonejs": "follow @keystonejs"
 }
