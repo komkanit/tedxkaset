409236f35c057be78b078328ea94b9d526f21abf
baivarn-tjr <t.sukkree@hotmail.com>
2017-04-06 23:14:29 +0700

    fix css

--- locales/en.js
+++ locales/en.js
@@ -7,6 +7,11 @@
 		"what_is_tedx": {
 			"header": "What is TEDx?",
 			"content": "<p>Lorem ipsum dolor sit amet, ius probo hendrerit te, eam blandit ocurreret elaboraret te, tota admodum te mel. Eum aeterno sapientem ut. Rebum labores rationibus ei sit. Vim at facilisi elaboraret, ad vel odio aliquam labores. Pri an vocibus accusam electram, et mel noluisse intellegebat.</p><p>Aliquip bonorum repudiandae ne sed, ea novum discere duo. Ipsum putent ut pro, stet fabulas ea vix. Ex prima persius has. Rebum antiopam sit et, mei quando salutandi te. Populo lucilius expetenda an quo. Eam te soluta accusamus conceptam. Vel enim dolorem salutatus an, sumo idque eu mel, mei te decore expetendis deterruisset.</p>"
-		}
+		},
+		"content": "event.content"
+	},
+	"aboutus": {
+		"content": "aboutus.content",
+		"question": "aboutus.question"
 	}
 }
