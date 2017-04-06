f413fe4401cbc3b37a3215cddc4b1d5e4746156d
mameaw14 <pinyapornster@gmail.com>
2017-04-06 22:41:25 +0700

    add content structure

--- locales/en.js
+++ locales/en.js
@@ -3,10 +3,47 @@
 		"read_more": "Read more"
 	},
 	"event": {
-		"speakers": "Speakers",
-		"what_is_tedx": {
-			"header": "What is TEDx?",
-			"content": "<p>Lorem ipsum dolor sit amet, ius probo hendrerit te, eam blandit ocurreret elaboraret te, tota admodum te mel. Eum aeterno sapientem ut. Rebum labores rationibus ei sit. Vim at facilisi elaboraret, ad vel odio aliquam labores. Pri an vocibus accusam electram, et mel noluisse intellegebat.</p><p>Aliquip bonorum repudiandae ne sed, ea novum discere duo. Ipsum putent ut pro, stet fabulas ea vix. Ex prima persius has. Rebum antiopam sit et, mei quando salutandi te. Populo lucilius expetenda an quo. Eam te soluta accusamus conceptam. Vel enim dolorem salutatus an, sumo idque eu mel, mei te decore expetendis deterruisset.</p>"
+		"content": {
+			"what_is_tedx": {
+				"slug": "what_is_tedx",
+				"header": "What is TEDx?",
+				"content": "content"
+			},
+			"event_info": {
+				"slug": "event_info",
+				"header": "Event Info",
+				"content": "event.event_info.content"
+			},
+			"speakers": {
+				"slug": "speakers",
+				"header": "Speakers",
+				"content": ""
+			}
 		}
+	},
+	"aboutus": {
+		"content": {
+			"team": {
+				"slug": "team",
+				"header": "Team",
+				"content": "aboutus.team.content"
+			},
+			"info": {
+				"slug": "info",
+				"header": "Info",
+				"content": "aboutus.info.content"
+			},
+			"contact_us": {
+				"slug": "contact_us",
+				"header": "Contact Us",
+				"content": "aboutus.contact_us.content"
+			}
+		},
+		"question": [
+			{
+				"ask": "ask",
+				"ans": "ans"
+			}
+		]
 	}
 }
--- locales/th.js
+++ locales/th.js
@@ -3,10 +3,47 @@
 		"read_more": "index.read_more"
 	},
 	"event": {
-		"what_is_tedx": {
-			"header": "event.what_is_tedx.header",
-			"content": "event.what_is_tedx.content"
+		"content": {
+			"what_is_tedx": {
+				"slug": "what_is_tedx",
+				"header": "What is TEDx?",
+				"content": "content"
+			},
+			"event_info": {
+				"slug": "event_info",
+				"header": "Event Info",
+				"content": "event.event_info.content"
+			},
+			"speakers": {
+				"slug": "speakers",
+				"header": "Speakers",
+				"content": ""
+			}
+		}
+	},
+	"aboutus": {
+		"content": {
+			"team": {
+				"slug": "team",
+				"header": "Team",
+				"content": "aboutus.team.content"
+			},
+			"info": {
+				"slug": "info",
+				"header": "Info",
+				"content": "aboutus.info.content"
+			},
+			"contact_us": {
+				"slug": "contact_us",
+				"header": "Contact Us",
+				"content": "aboutus.contact_us.content"
+			}
 		},
-		"speakers": "event.speakers"
+		"question": [
+			{
+				"ask": "ask",
+				"ans": "ans"
+			}
+		]
 	}
 }
